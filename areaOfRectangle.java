d78d6c357cd528888b0a49cdafe5584de24d6244
Sabyasachi <krram368@gmail.com>
2026-07-31 01:51:33 +0530

    RectangleArea

---
+++ areaOfRectangle.java
@@ -0,0 +1,25 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class areaOfRectangle {
+    static void main(String[] args) {
+        //Q= take length and breadth of rectangle as input and write a program to find whether the area of rectangle is greater than its parameter
+
+        Scanner sc = new Scanner(System.in);
+//       double l = sc.nextDouble();
+//       double b = sc.nextDouble();
+//
+//       double area = l*b;
+//       double perimeter = 2*(l+b);
+//       if (area>perimeter){
+//           System.out.println("Area is greater than perimeter");
+//       }
+//       else if (area == perimeter) {
+//           System.out.println("Area is equal to perimeter");
+//       }
+//       else {
+//           System.out.println("Area is not greater than perimeter");
+//       }
+    }
+}
