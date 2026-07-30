6ca2cbca75397ebf5f5149d8dc46e2e6ea4a189b
Sabyasachi <krram368@gmail.com>
2026-07-31 01:51:09 +0530

    AbsoluteNumber

---
+++ absoluteNumber.java
@@ -0,0 +1,26 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class absoluteNumber {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //ABSOLUTE NUMBER
+//       System.out.println("Enter the number: ");
+//       int n = sc.nextInt();
+//       if(n<0){
+//           n = -n;
+//       }
+//       System.out.println(n);
+
+//       IS INTEGER
+//        double n = sc.nextDouble();
+//        double x = (int)n;
+//        if(n-x == 0){
+//            System.out.println("Is an Integer");
+//       }
+//        else{
+//            System.out.println("Not an Integer");
+//        }
+    }
+}
