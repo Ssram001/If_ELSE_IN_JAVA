c7bc85d2bbb352c870fb464e50b69a6624e15172
Sabyasachi <krram368@gmail.com>
2026-08-01 22:23:20 +0530

    CostPrise_SellingPrice

---
+++ cp_SP.java
@@ -0,0 +1,33 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class cp_SP {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //       Question
+//       System.out.print("Enter the cost price: ");
+//       double cp = sc.nextDouble();
+//       System.out.print("Enter the selling price: ");
+//       double sp = sc.nextDouble();
+//
+//       if(sp>cp){
+//          double profit = sp - cp;
+//           System.out.println("The profit is: "+profit);
+//           double PP = ((sp - cp)/cp)*100;
+//           System.out.println("The profit percentage is: "+PP);
+//
+//       }
+//       else if(cp>sp){
+//           double loss = cp- sp;
+//           System.out.println("The LOSS is: "+loss);
+//           double LP = ((cp-sp)/cp)*100;
+//           System.out.println("The LOSS percentage is: "+LP);
+//
+//       }
+//       else{
+//           System.out.println("Neither loss nor profit");
+//       }
+
+    }
+}
