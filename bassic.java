8176539e53d4f2bdc1b39a80946d4a7ef25b808b
Sabyasachi <krram368@gmail.com>
2026-08-01 22:22:44 +0530

    basic

---
+++ bassic.java
@@ -0,0 +1,56 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class bassic {
+   public static void main(String[] args) {
+
+//        we cant store boolean = 0 or 1 like c++, only we store any condition,true or false
+       Scanner sc = new Scanner(System.in);
+
+
+//       System.out.print("enter the number: ");
+//       double n = sc.nextDouble();
+//        if(n>999 && n<10000){
+//            System.out.println(n + "is a four digit number");
+//       }
+//        else{
+//            System.out.println(n + "is not a four digit number");
+//        }
+
+        //take integer input and tell if its magnitude is smaller than 69 or not
+//       int n = sc.nextInt();
+//       if(n<69){
+//           System.out.println("true");
+//       }
+//       else if (n<0){
+//           n = -n;
+//           if (n<69){
+//               System.out.println("true");
+//           }
+//       }
+//       else{
+//           System.out.println("False");
+//       }
+
+       //or
+//       int n = sc.nextInt();
+//
+//       if (Math.abs(n) < 69) {
+//           System.out.println("true");
+//       } else {
+//           System.out.println("false");
+//       }
+
+
+ //
+
+
+
+
+
+
+
+       
+   }
+}
