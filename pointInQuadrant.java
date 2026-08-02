f0d0f1f84dc3efb42135101b3b1ace2812d4c70e
Sabyasachi <krram368@gmail.com>
2026-08-02 23:45:37 +0530

    point_in_quadrant

---
+++ pointInQuadrant.java
@@ -0,0 +1,28 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class pointInQuadrant {
+    static void main(String[] args) {
+
+        //Q = point and quadrant point
+
+        Scanner sc = new Scanner(System.in);
+        //       double x = sc.nextDouble();
+//       double y = sc.nextDouble();
+//       if (x>0 && y>0){
+//           System.out.println("The point lie in the first quadrant");
+//       } else if (x < 0 && y>0) {
+//           System.out.println("The point lie in the 2nd quadrant");
+//       }
+//       else if (x>0 && y<0){
+//           System.out.println("The point lie in the 3rd quadrant");
+//       }
+//       else if (x<0 && y<0){
+//           System.out.println("The point lie in the 4th quadrant");
+//       }
+//       else {
+//           System.out.println("The point lie in the origin");
+//       }
+    }
+}
