2c3a13e58d4c487bc42df0040e87f2ec453914fd
Sabyasachi <krram368@gmail.com>
2026-08-02 23:45:07 +0530

    greatestOFthree

---
+++ greatestAmongThre.java
@@ -0,0 +1,32 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class greatestAmongThre {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Q - greatest among three
+//       double a = sc.nextDouble();
+//       double b = sc.nextDouble();
+//       double c = sc.nextDouble();
+//
+//       if (a>b){
+//           if (a>c){
+//               System.out.println("a is greater");
+//           }
+//           else {
+//               if (b>c){
+//                   System.out.println("b is greater");
+//               }
+//              else{
+//                   System.out.println("c is greater");
+//               }
+//           }
+//       }
+
+
+        // Ternary operator
+//      double max = (a>b) ? ((a>c)? a : c) : ((b > c) ? b : c);
+//       System.out.println(max);
+    }
+}
